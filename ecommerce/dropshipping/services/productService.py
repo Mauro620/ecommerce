78a8c91939d0c0f96cd98f79aa1e7a7f4cc0e017
Celis620 <mauro.celis620@gmail.com>
2025-04-24 09:47:31 -0500

    Arreglos en las tasas de cambio

--- ecommerce/dropshipping/services/productService.py
+++ ecommerce/dropshipping/services/productService.py
@@ -25,9 +25,15 @@ def get_product_reviews_data(product):
 
 def convert_product(request, product):
     selected_currency = request.session.get("currency", "COP")
-
     converted_price = convert_currency(product.end_price, "COP", selected_currency)
     converted_offer_price = convert_currency(product.offer_price, "COP", selected_currency)
+
+    if selected_currency == "COP" or selected_currency == "ARS" or selected_currency == "CLP":
+        product.converted_price = round(converted_price)
+        product.converted_offer_price = round(converted_offer_price)
+        product.currency = selected_currency
+        return product
+    
     product.converted_price = round(converted_price, 2)
     product.converted_offer_price = round(converted_offer_price, 2)
     product.currency = selected_currency
--- ecommerce/dropshipping/views/indexView.py
+++ ecommerce/dropshipping/views/indexView.py
@@ -5,6 +5,7 @@ from django.shortcuts import redirect
 from dropshipping.utils import convert_currency
 from dropshipping.utils.formatters import round_to_1_decimal
 from django.db.models.functions import Round
+from dropshipping.services.productService import convert_product
 
 def index(request):
     products = Products.objects.prefetch_related('images').annotate(
@@ -13,12 +14,7 @@ def index(request):
     selected_currency = request.session.get("currency", "COP")
 
     for product in products:
-        original_price = product.end_price 
-        converted_price = convert_currency(original_price, "COP", selected_currency)
-        converted_offer_price = convert_currency(product.offer_price, "COP", selected_currency)
-        product.converted_offer_price = round(converted_offer_price, 2)
-        product.converted_price = round(converted_price, 2)
-        product.currency = selected_currency
+        convert_product(request, product)
 
     for product in products:
         product.image_pairs = [
